63b6711e01817d92da27ae42fb963db55a3587ac
sergeycin <67867645+sergeycin@users.noreply.github.com>
2021-11-15 16:37:22 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {UserList} from "./components/UserList"
 
 function App() {
   return (
     <div>
-
+<UserList/>
     </div>
   );
 }
---
+++ src/components/UserList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+export const UserList: React.FC = () => {
+    const state = useSelector(state => state)
+    return (
+        <div>
+
+        </div>
+    );
+}
+
